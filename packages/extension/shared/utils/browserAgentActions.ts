ccfd641532dc4848d8fa87743f312941c62b1ee9
Raghav Misra <raghav.m2014@gmail.com>
2025-11-03 17:18:56 -0500

    cut screenshot delay

--- packages/extension/shared/utils/browserAgentActions.ts
+++ packages/extension/shared/utils/browserAgentActions.ts
@@ -517,7 +517,7 @@ export const browserAgentActions = {
     }
   },
   async CAPTURE_SCREENSHOT(tabId: number) {
-    await waitMs(1500); // wait a bit for rendering
+    await waitMs(500); // wait a bit for rendering
     // Capture screenshot of the visible viewport
     const { data } = await sendCommand<Protocol.Page.CaptureScreenshotResponse>(
       tabId,
@@ -535,7 +535,7 @@ export const browserAgentActions = {
   },
   async WAIT() {
     // Simply wait for 1 second
-    await waitMs(1000);
+    await waitMs(500);
     return { success: true, info: "Waited" };
   },
   async SCROLL_DOCUMENT(tabId: number, { direction, magnitude }) {
